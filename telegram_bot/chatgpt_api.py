30b7c060fc1bc24d87696b553f41b1ac06b1c6f0
Deparok <146764485+Deparok@users.noreply.github.com>
2024-03-02 04:38:36 +0500

    Add files via upload

---
+++ telegram_bot/chatgpt_api.py
@@ -0,0 +1,2 @@
+def something():
+    return 'You asked ChatGPT for something and he said, quote: "fuck you!"'
---
+++ telegram_bot/tg_bot.py
@@ -0,0 +1,60 @@
+import telebot
+import telebot.util as util
+# import time
+import telegram_bot.chatgpt_api as chatgpt
+
+token = '6609693380:AAF56yGszmmChFfGniQtPNO5OiSNlJHC4WQ'
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start', 'help', 'site', 'gpt', 'timetable', 'inside', 'stop'])
+def bot_commands(command):
+    role = bot.get_chat_member(command.chat.id, command.from_user.id).status
+
+    if util.extract_command(command.text) == 'start':
+        bot.send_message(command.chat.id, "Напиши /help для получения информации о моих командах")
+    elif util.extract_command(command.text) == 'help':
+        bot.send_message(command.chat.id, "/help - справка\n/site - для перехода на сайт"
+        "(в разработке)\n/gpt *** - запрос в ChatGPT\n/timetable - расписание занятий(в разработке)"
+        "\n/inside - поиск внутри группы(в разработке)\n/stop - для остановки бота")
+    elif util.extract_command(command.text) == 'site':
+        bot.send_message(command.chat.id, "СКАЗАНО ЖЕ БЛЯТЬ 'site' В РАЗРАБОТКЕ")
+    elif util.extract_command(command.text) == 'gpt':
+        bot.send_message(command.chat.id, chatgpt.something())
+    elif util.extract_command(command.text) == 'timetable':
+        bot.send_message(command.chat.id, "СКАЗАНО ЖЕ БЛЯТЬ 'timetable' В РАЗРАБОТКЕ")
+    elif util.extract_command(command.text) == 'inside':
+        bot.send_message(command.chat.id, "СКАЗАНО ЖЕ БЛЯТЬ 'inside' В РАЗРАБОТКЕ")
+    elif util.extract_command(command.text) == 'stop':
+        print(role)
+        print(command.chat.type)
+        if role in ['administrator',
+                    'creator'] or command.chat.type == 'private':  # role = member, administrator, creator     #type = private, group, supergroup, channel
+            bot.send_message(command.chat.id, "Остановка бота . . .")
+            bot.stop_bot()
+            bot.send_message(command.chat.id, "Бот успешно остановлен")
+        else:
+            bot.send_message(command.chat.id, "У вас нет прав на остановку бота")
+    else:
+        bot.send_message(command.chat.id, "Введена неизвестная команда, напиши /help")
+
+
+# @bot.message_handler(content_types=['text', 'document', 'audio'])
+# def get_text_messages(message):
+#     if message.text == "Привет":
+#         bot.send_message(message.chat.id, "Привет -_-")
+#         time.sleep(1.5)
+#         bot.send_message(message.chat.id, "Помощь нужна?")
+#         time.sleep(3)
+#         bot.send_message(message.chat.id, "/start пиши, хуль сидишь")
+start = telebot.types.BotCommand('/start', "начать общение")
+help = telebot.types.BotCommand('/help', "список команд")
+site = telebot.types.BotCommand('/site', "открыть сайт")
+gpt = telebot.types.BotCommand('/gpt', "поиск в ChatGPT")
+timetable = telebot.types.BotCommand('/timetable', "расписание МГТУ")
+inside = telebot.types.BotCommand('inside', "поиск внутри группы")
+stop = telebot.types.BotCommand('stop', "остановить бота можно с правами администратора и выше")
+
+bot.set_my_commands([start, help, site, gpt, timetable, inside, stop])
+
+bot.infinity_polling()
