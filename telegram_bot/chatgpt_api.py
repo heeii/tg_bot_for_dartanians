08578b853428b8ad879fb67db0d4a8b6b9da9798
heeii <72515845+heeii@users.noreply.github.com>
2024-03-04 20:52:07 +0500

    with gpt

--- telegram_bot/chatgpt_api.py
+++ telegram_bot/chatgpt_api.py
@@ -1,2 +1,12 @@
-def something():
-    return 'You asked ChatGPT for something and he said, quote: "fuck you!"'
+import openai
+import json
+def request(prompt):
+    with open('setings.json', 'r') as f:
+        config = json.load(f)
+    openai.api_key = config["gpt_api_key"]
+    engine = "text-davinci-003"
+    completion = openai.Completion.create(engine=engine,
+                                          prompt=prompt,
+                                          temperature=0.5,
+                                          max_tokens=1000)
+    return completion.choices[0]['text']
